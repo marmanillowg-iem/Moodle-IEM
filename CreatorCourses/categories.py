9578f8f5cf1def38cdb8d0838f649124ec705e6f
GabrielUnsa <gab24wal@gmail.com>
2026-03-16 22:12:02 -0300

    se crearonlas nuevas aulas y modificaron las categorias para que quede visible solo los del 2026. Falta solo matricular a los estudiantes y profesores.

--- CreatorCourses/categories.py
+++ CreatorCourses/categories.py
@@ -1,5 +1,16 @@
 from api import moodle_call
 
+diccionary_categories = {
+    1: "Sin categoría",
+    2: "Primaria",
+    3: "Secundaria",
+    4: "Superior",
+    5: "Administración",
+    6: "Docentes",
+    7: "Alumnos",
+    8: "Otros"
+}
+
 def get_all_categories():
     return moodle_call("core_course_get_categories")
 
@@ -10,5 +21,17 @@ def get_category_by_id(categoryid):
     )
 
 def view_categories():
-    for category in get_all_categories():
-        print(f"ID: {category.get('id')} - Name: {category.get('name')}")
+    for category in sorted(get_all_categories(), key=lambda x: x.get('id', 0)):
+        print(f"ID: {category.get('id')} - Name: {category.get('name')} - Parent ID: {category.get('parent')} - Description: {category.get('description')}")
+
+def view_only_category_with_parent_id():
+    for category in sorted(get_all_categories(), key=lambda x: x.get('id', 0)):
+        if category.get('parent') != 0:
+            print(f"ID: {category.get('id')} - Name: {category.get('name')} - Parent ID: {category.get('parent')} - Description: {category.get('description')}")
+            
+def category_with_parent_id():
+    diccionary_categories_with_parent_id = {}
+    for category in sorted(get_all_categories(), key=lambda x: x.get('id', 0)):
+        if category.get('parent') != 0:
+            diccionary_categories_with_parent_id[category.get('parent')] = category.get('id')
+    return diccionary_categories_with_parent_id
--- CreatorCourses/courses.py
+++ CreatorCourses/courses.py
@@ -15,14 +15,13 @@ def filter_courses_by_age(courses, year_suffix):
             filtered_courses.append(course)
     return filtered_courses
 
-def create_course(fullname, shortname, description, categoryid, displayname):
+def create_course(fullname, shortname, description, categoryid):
     try:
         response = moodle_call('core_course_create_courses', {
             'courses[0][fullname]': fullname,
             'courses[0][shortname]': shortname,
             'courses[0][summary]': description,
             'courses[0][categoryid]': categoryid,
-            'courses[0][displayname]': displayname
         })
         return response
     except Exception as e:
@@ -35,19 +34,39 @@ def view_course_details(courses):
         print(f"Indice: {i} - ShortName: {course.get('shortname')} - Fullname: ({course.get('fullname')}) - Categoría ID: {course.get('categoryid')} - summary: {course.get('summary')}")
         i += 1
 
-def preview_course_to_create(course, year_suffix):
+def create_list_courses(courses):
+    results = []
+    for course in courses:
+        results.append(create_course( course.get('fullname'), course.get('shortname'), course.get('summary'), course.get('categoryid')))
+    return results
+
+def course_to_create(course, year_suffix):
+    courses_to_create = []
     for course in course:
-        print(f"Curso a crear: ShortName: {course.get('shortname')[:-2] + year_suffix} - Fullname: ({course.get('fullname')[:-2] + year_suffix}) - Categoría ID: {course.get('categoryid')} - summary: {course.get('summary')[:-2]+year_suffix} - displayname: {course.get('displayname')}")
-        
-# def comparated(a, b):
-#     ca=0
-#     cb=0
-#     for ab in a:
-#         if ab.get('shortname','').strip()[:-3] in [bb.get('shortname','').strip()[:-3] for bb in b]:
-#             ca += 1
-#             print(f"Curso existente: {ab.get('shortname')} - {ab.get('fullname')}")
-#         else:
-#             print(f"Curso nuevo a crear: {ab.get('shortname')} - {ab.get('fullname')}")
-#             cb += 1
-#     print(f"Total cursos existentes: {ca}")
-#     print(f"Total cursos nuevos a crear: {cb}")
+        courses_to_create.append({
+            'shortname': course.get('shortname')[:-2] + year_suffix,
+            'fullname': course.get('fullname')[:-2] + year_suffix,
+            'categoryid': course.get('categoryid'),
+            'summary': 'Aula Virtual de ' + course.get('fullname')[:-2] + year_suffix,
+            'displayname': course.get('fullname')[:-2] + year_suffix,
+        })
+    return courses_to_create
+
+def modify_course_category(courseid, new_categoryid):
+    try:
+        response = moodle_call('core_course_update_courses', {
+            'courses[0][id]': courseid,
+            'courses[0][categoryid]': new_categoryid,
+        })
+        return response
+    except Exception as e:
+        print(f"Error al modificar curso: {e}")
+        return None
+
+def modify_courses_categories(courses, new_categories):
+    results = []
+    for course in courses:
+        course_category_id = course.get('categoryid')
+        if course_category_id in new_categories:
+            results.append(modify_course_category(course.get('id'), new_categories[course_category_id]))
+    return results
